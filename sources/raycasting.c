1c07f8618fbea3b753fb8e0bbdfd89f6a7d7ec61
lamPolar <lovast7@gmail.com>
2022-11-01 18:52:30 +0900

    11/1 [BUG] add new file to makefile

--- sources/raycasting.c
+++ sources/raycasting.c
@@ -1,5 +1,13 @@
 #include "cub3d.h"
 
+void my_mlx_pixel_put(t_img *data, int x, int y, int color){
+	char *dst;
+
+	dst = data->addr + (y * data -> line_length + x *( data->bits_per_pixel / 8));
+	//괄호 = offset
+	*(unsigned int *) dst = color;
+}
+
 void do_mlx(void)
 {
     t_mlx	mlx;
@@ -12,5 +20,11 @@ void do_mlx(void)
 	mlx_key_hook(mlx.win_ptr, key_hook, &mlx);
 	mlx_hook(mlx.win_ptr, 17, 1L << 5, close_window, &mlx);
 	mlx_put_image_to_window(mlx.mlx_ptr, mlx.win_ptr, mlx.img.img_ptr, 0, 0);
+    for(int i = 0; i < 100 ; i++) {
+	my_mlx_pixel_put(&mlx.img, i, i, 0x00FF0000);// 붉은색 선을 대각선으로 그린다.
+	my_mlx_pixel_put(&mlx.img, 5, i, 0x00FF0000);// 붉은색 선을 세로으로 그린다.
+	my_mlx_pixel_put(&mlx.img, i, 5, 0x00FF0000);// 붉은색 선을 가로으로 그린다.
+	}
 	mlx_loop(mlx.mlx_ptr);
 }
+
